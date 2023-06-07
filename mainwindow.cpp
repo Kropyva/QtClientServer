a39f870ca5c607a76999ddae9944656de2a0548b
Kropyva <anatolii.kropyvka@gmail.com>
2023-06-07 04:48:28 +0300

    Added an event handler that handles changes in a textVector and matches it with the regular expression.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,12 +4,27 @@
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
+    , regExp(new QRegularExpression("^((\\d+(\\s?))+)$"))
 {
     ui->setupUi(this);
+
+    QObject::connect(ui->textVector, &QTextEdit::textChanged, this, [this](){
+        QString text { ui->textVector->toPlainText() };
+        QRegularExpressionMatch match { regExp->match(text)};
+
+        if (!match.hasMatch()) {
+            QTextCursor cursor = ui->textVector->textCursor();
+
+            cursor.movePosition(QTextCursor::End);
+            cursor.movePosition(QTextCursor::Left, QTextCursor::KeepAnchor);
+            cursor.removeSelectedText();
+        }
+    });
 }
 
 MainWindow::~MainWindow()
 {
+    delete regExp;
     delete ui;
 }
 
