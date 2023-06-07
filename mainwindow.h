573c5a0295cf0d6b9a59db2dca65b2f223e21c2e
Kropyva <anatolii.kropyvka@gmail.com>
2023-06-07 04:43:29 +0300

    Added a pointer to reqular expression

--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QRegularExpression>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -17,5 +18,6 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    QRegularExpression *regExp;
 };
 #endif // MAINWINDOW_H
